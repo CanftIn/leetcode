bc9140f70f1d1c21f34f16c0524e0b7d4f1eef0e
CanftIn <wwc7033@gmail.com>
2018-01-22 12:15:11 +0800

    35_Search_Insert_Position
    35_Search_Insert_Position

--- 35_Search_Insert_Position/35_Search_Insert_Position/35_Search_Insert_Position.cpp
+++ 35_Search_Insert_Position/35_Search_Insert_Position/35_Search_Insert_Position.cpp
@@ -2,8 +2,26 @@
 #include <vector>
 
 using namespace std;
+/*
+//others solution
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+        while (low <= high) {
+            int mid = low + (high-low)/2;
 
+            if (nums[mid] < target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return low;
+    }
+};
+*/
 
+//my solution
 class Solution {
 public:
 	int searchInsert(vector<int>& nums, int target) {
