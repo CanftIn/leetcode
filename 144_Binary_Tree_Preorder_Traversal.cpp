06b08e9c72e5eb6d2c00b2ce6e60137a424a26fa
CanftIn <wwc7033@gmail.com>
2018-05-29 23:27:57 +0800

    update

--- 144_Binary_Tree_Preorder_Traversal.cpp
+++ 144_Binary_Tree_Preorder_Traversal.cpp
@@ -29,3 +29,34 @@ private:
         helper(res, root->right);
     }
 };
+
+// LeetCode, Binary Tree Preorder Traversal
+// Morris O(n) O(1)
+class Solution1 {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> result;
+        TreeNode *cur = root, *prev = nullptr;
+        while (cur != nullptr) {
+            if (cur->left == nullptr) {
+                result.push_back(cur->val);
+                prev = cur;
+                cur = cur->right;
+            } else {
+                TreeNode *node = cur->left;
+                while (node->right != nullptr && node->right != cur)
+                    node = node->right;
+                if (node->right == nullptr) {
+                    result.push_back(cur->val);
+                    node->right = cur;
+                    prev = cur;
+                    cur = cur->left;
+                } else {
+                    node->right = nullptr;
+                    cur = cur->right;
+                }
+            }
+        }
+        return result;
+    }
+};
--- 94_Binary_Tree_Inorder_Traversal.cpp
+++ 94_Binary_Tree_Inorder_Traversal.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
-#include <vector>
 
-using namespace std;
-
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TrinULL)1  {}
 };
 
 class Solution {
@@ -27,5 +18,45 @@ private:
         helper(res, root->left);
         res.push_back(root->val);
         helper(res, root->right);
+    }
+};
+
+    void helper(vector<int>& res, TreeNode* root)
+    {
+        if(root == NULL) return ;
+        helper(res, root->left);
+        res.push_back(root->val);
+        helper(res, root->right);
+    }
+};
+
+// LeetCode, Binary Tree Inorder Traversal
+// Morris O(n)  O(1)
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> result;
+        TreeNode *cur = root, *prev = nullptr;
+        while (cur != nullptr) {
+            if (cur->left == nullptr) {
+                result.push_back(cur->val);
+                prev = cur;
+                cur = cur->right;
+            } else {
+                TreeNode *node = cur->left;
+                while (node->right != nullptr && node->right != cur)
+                    node = node->right;
+                if (node->right == nullptr) {
+                    node->right = cur;
+                    cur = cur->left;
+                } else {
+                    result.push_back(cur->val);
+                    node->right = nullptr;
+                    prev = cur;
+                    cur = cur->right;
+                }
+            }
+        }
+        return result;
     }
 };
