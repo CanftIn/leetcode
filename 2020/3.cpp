207cdd33fe06c2f2eef8e528c82bf2f77ec4314e
CanftIn <wwc7033@gmail.com>
2020-04-30 23:42:08 +0800

    Create 3.cpp

---
+++ 2020/3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.size() == 0) return 0;
+        unordered_set<char> lookup;
+        int maxStr = 0;
+        int left = 0;
+        for(int i = 0; i < s.size(); i++) {
+            while (lookup.find(s[i]) != lookup.end()){
+                lookup.erase(s[left]);
+                left ++;
+            }
+            maxStr = max(maxStr,i-left+1);
+            lookup.insert(s[i]);
+        }
+        return maxStr;
+    }
+};
