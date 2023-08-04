ad67d4dc2a3e245b9a7689de10848226d83f7127
CanftIn <wwc7033@gmail.com>
2023-08-04 10:55:11 +0800

    restart 2023

--- 2023/1/so.cpp
+++ 2023/1/so.cpp
@@ -1,13 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mymap;
+        unordered_map<int, int> map_num;
         for (int i = 0; i < nums.size(); ++i) {
-            if (mymap.find(target - nums[i]) != mymap.end()) {
-                return {mymap.find(target - nums[i])->second, i};
+            if (map_num.find(target - nums[i]) != map_num.end()) {
+                return {map_num[target - nums[i]], i};
             }
-            mymap[nums[i]] = i;
+            map_num.insert({nums[i], i});
         }
         return {};
     }
 };
+// @lc code=end
