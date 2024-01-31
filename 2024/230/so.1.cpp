265441eae0805eb619b56cbddbf5eec34b2c8e16
CanftIn <wwc7033@gmail.com>
2024-01-31 17:13:54 +0800

    add 230 so

---
+++ 2024/230/so.1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        this->k = k;
+        dfs(root);
+        return res;
+    }
+
+private:
+    int res, k;
+    void dfs(TreeNode* root) {
+        if (root == nullptr) return;
+        dfs(root->left);
+        if (k == 0) return;
+        if (--k == 0) res = root->val;
+        dfs(root->right);
+    }
+};
