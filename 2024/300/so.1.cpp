1ea5cd5f3842e3267e7449b2f19e66b088314e6b
CanftIn <wwc7033@gmail.com>
2024-02-02 11:10:23 +0800

    add 300 so

---
+++ 2024/300/so.1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < dp.size(); ++i) {
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
